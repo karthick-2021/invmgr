ddc8c8fd8afac2b12d91f94a4f6971eef2711835
karthick-2021 <92563415+karthick-2021@users.noreply.github.com>
2022-01-01 10:55:20 +0530

    cors fix 2

--- src/main/java/com/invmgr/invmgr/controllers/BillingsController.java
+++ src/main/java/com/invmgr/invmgr/controllers/BillingsController.java
@@ -4,14 +4,12 @@ import com.invmgr.invmgr.domain.Inventory;
 import com.invmgr.invmgr.domain.Invoice;
 import com.invmgr.invmgr.service.BillingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class BillingsController {
 
     @Autowired
